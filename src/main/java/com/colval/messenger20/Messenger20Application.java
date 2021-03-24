f5fbc19fa5c7e4ccc40d155ce7e3d825c2c646d1
Emerik Latour <emeriklatour@gmail.com>
2021-03-24 15:58:45 -0400

    first commit

---
+++ src/main/java/com/colval/messenger20/Messenger20Application.java
@@ -0,0 +1,13 @@
+package com.colval.messenger20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Messenger20Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Messenger20Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/colval/messenger20/Messenger20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colval.messenger20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Messenger20ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
