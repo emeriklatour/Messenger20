a171397db927594ac6e576fd4a3b18a9ff1f6fa8
Emerik Latour <emeriklatour@gmail.com>
2021-05-03 14:08:56 -0400

    heroku

--- src/main/java/com/colval/messenger20/services/IConversationService.java
+++
@@ -1,7 +0,0 @@
-package com.colval.messenger20.services;
-
-import com.colval.messenger20.model.entities.Conversations;
-
-public interface IConversationService {
-    Conversations readOne(int conversationId);
-}
--- src/main/java/com/colval/messenger20/services/IMessageService.java
+++
@@ -1,9 +0,0 @@
-package com.colval.messenger20.services;
-
-import com.colval.messenger20.model.DTO.MessageDto;
-import com.colval.messenger20.model.entities.Messages;
-
-public interface IMessageService {
-
-    Messages create(MessageDto message, String username);
-}
